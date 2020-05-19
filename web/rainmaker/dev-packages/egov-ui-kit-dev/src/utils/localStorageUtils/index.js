d7447e27ea535be34d5c0d654883cf6e01c7d327
Chandra Kamal <chandra.kamal1@gmail.com>
2020-05-19 08:59:36 +0530

    init: initial commit

---
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/localStorageUtils/index.js
@@ -0,0 +1,126 @@
+
+
+const appName = process.env.REACT_APP_NAME;
+
+//GET methods
+export const getAccessToken = () => {
+  return localStorageGet(`token`);
+};
+export const getUserInfo = () => {
+  return localStorageGet("user-info");
+};
+export const getTenantId = () => {
+  return localStorageGet("tenant-id");
+};
+export const getLocalization = (key) => {
+  return localStorage.getItem(key);
+};
+export const getLocale = () => {
+  return localStorage.getItem("locale");
+};
+
+export const getDefaultLocale = () => {
+  return localStorage.getItem("defaultLocale");
+};
+
+
+export const getapplicationType = () => {
+  return localStorage.getItem("applicationType");
+};
+
+export const getapplicationNumber = () => {
+  return localStorage.getItem("ApplicationNumber");
+};
+
+export const getapplicationMode = () => {
+  return localStorage.getItem("applicationMode");
+};
+
+export const getOPMSTenantId = () => {
+  return localStorage.getItem('opms-tenant-id');
+};
+
+
+//SET methods 
+export const setUserInfo = (userInfo) => {
+  localStorageSet("user-info", userInfo, null);
+};
+export const setAccessToken = (token) => {
+  localStorageSet("token", token, null);
+};
+export const setRefreshToken = (refreshToken) => {
+  localStorageSet("refresh-token", refreshToken, null);
+};
+export const setTenantId = (tenantId) => {
+  localStorageSet("tenant-id", tenantId, null);
+};
+export const setLocale = (locale) => {
+  localStorageSet("locale", locale);
+};
+export const setReturnUrl = (url) => {
+  localStorageSet("returnUrl", url);
+};
+
+export const setDefaultLocale = (locale) => {
+  localStorageSet("defaultLocale", locale);
+};
+
+export const setapplicationType = (applicationTypeName) => {
+  localStorageSet("applicationType", applicationTypeName);
+};
+
+export const setapplicationNumber = (applicationNumber) => {
+  localStorageSet("ApplicationNumber", applicationNumber);
+};
+
+export const setapplicationMode = (applicationMode) => {
+  localStorageSet("applicationMode", applicationMode);
+};
+export const setOPMSTenantId = (OPMSTenantId) => {
+  localStorageSet('opms-tenant-id',OPMSTenantId);
+};
+
+
+
+//Remove Items (LOGOUT)
+export const clearUserDetails = () => {
+  Object.keys(localStorage).forEach((key) => {
+    if (key.startsWith(appName)) {
+      window.localStorage.removeItem(key);
+    }
+  });
+};
+//Role specific get-set Methods
+export const localStorageGet = (key, path) => {
+  const appName = process.env.REACT_APP_NAME;
+  let value = null;
+  if (path) {
+    const data = JSON.parse(window.localStorage.getItem(appName + "." + key)) || null;
+    value = get(data, path);
+  } else {
+    value = window.localStorage.getItem(appName + "." + key) || null;
+  }
+  return value;
+};
+export const localStorageSet = (key, data, path) => {
+  const appName = process.env.REACT_APP_NAME;
+  const storedData = window.localStorage.getItem(appName + "." + key);
+
+  if (path) {
+    set(storedData, path, data);
+    window.localStorage.setItem(appName + "." + key, storedData);
+    window.localStorage.setItem( key, storedData);
+  } else {
+    window.localStorage.setItem(appName + "." + key, data);
+    window.localStorage.setItem(key, data);
+  }
+};
+//Remove Item
+export const lSRemoveItem = (key) => {
+  const appName = process.env.REACT_APP_NAME;
+  window.localStorage.removeItem(appName + "." + key);
+};
+
+export const lSRemoveItemlocal = (key) => {
+  window.localStorage.removeItem(key);
+};
